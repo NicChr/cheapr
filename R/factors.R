ea41cd8457a284be292dafd265f28721bfd75646
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2024-08-18 11:50:31 +0100

    New function and improvements.

--- R/factors.R
+++ R/factors.R
@@ -9,7 +9,9 @@
 #' `unused_levels()` which returns the unused levels of a factor,
 #' `add_na_level()` which adds an explicit `NA` level,
 #' `drop_na_level()` which drops the `NA` level,
-#' and `drop_levels()` which drops unused factor levels.
+#' `drop_levels()` which drops unused factor levels,
+#' and finally `reorder_levels()` which reorders the levels of `x`
+#' based on `y` using the ordered median values of `y` for each level.
 #'
 #' @returns
 #' A `factor` or `character` in the case of `used_levels` and `unused_levels`.
@@ -24,6 +26,10 @@
 #' @param ordered Should the result be an ordered factor?
 #' @param name Name of `NA` level.
 #' @param where Where should `NA` level be placed? Either first or last.
+#' @param order_by A vector to order the levels of `x` by using the medians of
+#' `order_by`.
+#' @param decreasing Should the reordered levels be in decreasing order?
+#' Default is `FALSE`.
 #'
 #' @details
 #' This operates similarly to `collapse::qF()`. \cr
@@ -39,9 +45,11 @@
 #'
 #' @export
 #' @rdname factors
-factor_ <- function(x = integer(), levels = NULL, order = TRUE,
-                    na_exclude = TRUE,
-                    ordered = is.ordered(x)){
+factor_ <- function(
+    x = integer(), levels = NULL, order = TRUE,
+    na_exclude = TRUE,
+    ordered = is.ordered(x)
+){
   if (is.null(x)){
     x <- integer()
   }
@@ -66,12 +74,60 @@ factor_ <- function(x = integer(), levels = NULL, order = TRUE,
   if (inherits(x, "POSIXt") && collapse::any_duplicated(fct_lvls)){
     fct_lvls <- paste(fct_lvls, as.POSIXlt(lvls)$zone)
   }
+  # if (!identical(levels, labels)){
+  #   fct_lvls <- as.character(labels)
+  # }
   attr(out, "levels") <- fct_lvls
-  class(out) <- c(if (ordered) "ordered" else character(),
-                  # if (!na_exclude) "na_included" else character(),
-                  "factor")
+  class(out) <- c(if (ordered) "ordered" else character(), "factor")
+  # class(out) <- c("cheapr_factor", if (ordered) "ordered" else character(),
+  #                 # if (!na_exclude) "na_included" else character(),
+  #                 "factor")
+  # attr(out, "data") <- x
   out
 }
+# print.cheapr_factor <- function(x, ...){
+#   class(x) <- setdiff(class(x), "cheapr_factor")
+#   attr(x, "data") <- NULL
+#   # class(x) <- setdiff(class(x), "cheapr_factor")
+#   # NextMethod("print")
+#   print(x, ...)
+# }
+# as.character.cheapr_factor <- function(x, ...){
+#   data <- attr(x, "data")
+#   if (!is.null(data)){
+#     x <- data
+#   }
+#   as.character(x, ...)
+#   # x <- attr(x, "data")
+#   # class(x) <- setdiff(class(x), "cheapr_factor")
+#   # NextMethod("as.character")
+# }
+# `[.cheapr_factor` <- function(x, ...){
+#   data <- attr(x, "data")
+#   attr(x, "data") <- NULL
+#   out <- NextMethod("[")
+#   if (!is.null(data)){
+#     attr(out, "data") <- data[...]
+#   }
+#   out
+# }
+#
+# as.double.cheapr_factor <- function(x, ...){
+#   data <- attr(x, "data")
+#   if (!is.null(data)){
+#     x <- data
+#   }
+#   as.double(x, ...)
+#   # NextMethod("as.numeric")
+# }
+# as.integer.cheapr_factor <- function(x, ...){
+#   data <- attr(x, "data")
+#   if (!is.null(data)){
+#     x <- data
+#   }
+#   as.integer(x, ...)
+#   # NextMethod("as.integer")
+# }
 #' @export
 #' @rdname factors
 levels_factor <- function(x){
@@ -87,15 +143,29 @@ check_is_factor <- function(x){
     stop("x must be a factor")
   }
 }
+factor_as_character <- function(x){
+  levels(x)[unclass(x)]
+}
+which_used_levels <- function(x){
+  which_in(levels_factor(x), x)
+}
+which_unused_levels <- function(x){
+  which_not_in(levels_factor(x), x)
+}
 #' @export
 #' @rdname factors
 used_levels <- function(x){
-  as.character(intersect_(levels_factor(x), x))
+  levels(x)[which_used_levels(x)]
+  # levels(x)[which_in(seq_along(levels(x)), unclass(x))]
+  # factor_as_character(intersect_(levels_factor(x), x))
 }
 #' @export
 #' @rdname factors
 unused_levels <- function(x){
-  as.character(setdiff_(levels_factor(x), x))
+  levels(x)[which_unused_levels(x)]
+  # levels(x)[which_not_in(seq_along(levels(x)), unclass(x))]
+  # factor_as_character(setdiff_(levels_factor(x), x))
+  # as.character(setdiff_(levels_factor(x), x))
 }
 #' @export
 #' @rdname factors
@@ -142,21 +212,75 @@ drop_na_level <- function(x){
     attr(out, "levels") <- new_lvls
 
     out
+
+    ### Alternative
+
+    # which_lvls <- seq_along(lvls)[-which_na_lvl]
+    # out <- which_lvls[unclass(x)]
+    #
+    # attributes(out) <- attributes(x)
+    # attr(out, "levels") <- levels(x)[which_lvls]
+    #
+    # out
   }
 }
+
+# Alternative
+# drop_levels <- function(x){
+#   lvls <- levels(x)
+#   n_lvls <- length(lvls)
+#   used_lvls <- intersect_(levels_factor(x), x)
+#   if (length(used_lvls) == n_lvls){
+#     x
+#   } else {
+#    factor_(x, levels = used_lvls)
+#     # sub alternative
+#     # out <- collapse::fmatch(lvls, used_lvls, overid = 2L)[unclass(x)]
+#     # attributes(out) <- attributes(x)
+#     # attr(out, "levels") <- as.character(used_lvls)
+#   }
+# }
+
 #' @export
 #' @rdname factors
 drop_levels <- function(x){
   lvls <- levels(x)
   n_lvls <- length(lvls)
-  used_lvls <- intersect_(levels_factor(x), x)
-  if (length(used_lvls) == n_lvls){
+  which_used <- which_used_levels(x)
+  if (length(which_used) == n_lvls){
     x
   } else {
-   factor_(x, levels = used_lvls)
-    # Alternative
-    # out <- collapse::fmatch(lvls, used_lvls, overid = 2L)[unclass(x)]
-    # attributes(out) <- attributes(x)
-    # attr(out, "levels") <- as.character(used_lvls)
+    # out <- collapse::fmatch(unclass(x), which_used)
+    out <- which_used[unclass(x)]
+    attributes(out) <- attributes(x)
+    attr(out, "levels") <- levels(x)[which_used]
+    out
   }
 }
+#' @export
+#' @rdname factors
+reorder_levels <- function(x, order_by, decreasing = FALSE){
+  check_is_factor(x)
+  groups <- collapse::GRP(x, return.groups = FALSE)
+  medians <- collapse::fmedian(order_by, g = groups, use.g.names = FALSE)
+
+  o <- collapse::radixorderv(medians, decreasing = decreasing)
+  sorted <- isTRUE(attr(o, "sorted"))
+  if (sorted){
+    x
+  } else {
+    ordered_levels <- levels(x)[o]
+    factor_(x, levels = ordered_levels)
+  }
+  # out <- collapse::fmatch(seq_along(levels(x)), o, overid = 2L)[unclass(x)]
+  # Alternative
+  # out <- collapse::fmatch(x, ordered_levels, overid = 2L)
+
+}
+
+# rename_levels <- function(x, names){
+#   check_length(names, length(levels(x)))
+#   levels(x) <- names
+#   x
+# }
+
