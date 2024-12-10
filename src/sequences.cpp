fb3fe52916d71093df56c0cd2371ce12978bf0f0
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2024-12-10 11:56:59 +0000

    Switch to ints.

--- src/sequences.cpp
+++ src/sequences.cpp
@@ -648,10 +648,10 @@ SEXP cpp_fixed_width_breaks(double start, double end, double n,
       seq_width = Rf_protect(Rf_ScalarReal(adj_width));
 
       out = Rf_protect(cpp_dbl_sequence(seq_size, seq_from, seq_width));
-      R_xlen_t n = n_breaks;
+      int seq_n = n_breaks;
       double *p_out = REAL(out);
       OMP_FOR_SIMD
-      for (R_xlen_t i = 0; i < n; ++i) p_out[i] = p_out[i] / scale_adj;
+      for (int i = 0; i < seq_n; ++i) p_out[i] = p_out[i] / scale_adj;
     }
 
     Rf_unprotect(4);
