06905043f46cf4a0e68115e23b65e66618fe7e0f
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2025-05-22 15:36:24 +0100

    Dependency fixes.

--- R/rebuild.R
+++ R/rebuild.R
@@ -26,7 +26,21 @@
 #' @rdname rebuild
 #' @export
 rebuild <- function(x, template, ...){
-  UseMethod("rebuild", template)
+  # Temporary piece of code to not break dependencies
+  if (inherits(template, c("episodes_tbl_df", "time_tbl_df"))){
+    old_class <- vec_intersect(class(template), c("episodes_tbl_df", "time_tbl_df"))
+    class(template) <- vec_setdiff(class(template), c("episodes_tbl_df", "time_tbl_df"))
+    out <- rebuild(x, template)
+
+    # Add extra attributes
+
+    extra_attrs <- list_drop_null(attributes(template)[c("time", "time_by", "threshold")])
+    attributes(out) <- c(attributes(out), extra_attrs)
+    class(out) <- c(old_class, class(out))
+    out
+  } else {
+    UseMethod("rebuild", template)
+  }
 }
 # rebuild.default <- function(x, template, shallow_copy = TRUE, ...){
 #   cpp_reconstruct(
