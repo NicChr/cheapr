a5ed1f115fccf75b32c62ce6e65c11ab6d8fb21f
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2025-05-26 09:37:18 +0100

    Use one package-level documentation script.

--- R/cheapr-package.R
+++ R/cheapr-package.R
@@ -1,3 +1,10 @@
+#' @description
+#' In this package, 'cheap' means fast and efficient.
+#'
+#' cheapr aims to provide a set of functions for programmers to write
+#' cheaper code, saving time and memory.
+"_PACKAGE"
+
 ## usethis namespace: start
 #' @useDynLib cheapr, .registration = TRUE
 ## usethis namespace: end
