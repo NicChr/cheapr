e943010bf21d7acd208474717655e4e9729b12d5
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-01-06 20:36:41 +0000

    Added factor constructor.

--- inst/include/cheapr/internal/r_vector.h
+++ inst/include/cheapr/internal/r_vector.h
@@ -9,6 +9,8 @@
 #include <cheapr/internal/r_vector_utils.h>
 #include <cheapr/internal/r_coerce.h>
 #include <unordered_map> // For string vector matching
+#include <unordered_set> // For unique strings
+#include <vector> // Also for unique strings
 
 namespace cheapr {
 
@@ -231,6 +233,35 @@ struct r_posixcts : public r_vec<r_dbl> {
 
 };
 
+r_vec<r_str> unique_strings(r_vec<r_str> x) {
+  R_xlen_t n = x.length();
+  
+  // Hash set for O(n) de-duplication
+  std::unordered_set<SEXP> seen;
+  seen.reserve(n);
+  
+  std::vector<r_str> unique_vec;
+  unique_vec.reserve(n / 2);
+  
+  for (R_xlen_t i = 0; i < n; ++i) {
+    r_str str = x.get(i);
+    if (seen.insert(str).second) {
+      unique_vec.push_back(str);
+    }
+  }
+  
+  // Result vector
+  R_xlen_t n_unique = unique_vec.size();
+  auto out = SHIELD(r_vec<r_str>(n_unique));
+  
+  for (R_xlen_t i = 0; i < n_unique; ++i) {
+    out.set(i, unique_vec[i]);
+  }
+  
+  YIELD(1);
+  return out;
+}
+
 
 // Match needle strings to first occurrence in haystack
 r_vec<r_int> string_match(r_vec<r_str> needles, r_vec<r_str> haystack) {
@@ -286,6 +317,13 @@ struct r_factors : public r_vec<r_int> {
     YIELD(2);
   }
 
+  explicit r_factors(r_vec<r_str> x){
+    auto levels = SHIELD(unique_strings(x));
+    auto fct = SHIELD(r_factors(x, levels));
+    this->value = fct.value;
+    YIELD(2);
+  }
+
   r_vec<r_str> levels_get() const {
     return r_vec<r_str>(attr::get_attr(this->value, internal::as_r<r_sym>("levels")));
   }
@@ -313,6 +351,8 @@ struct r_factors : public r_vec<r_int> {
 //   }
 // };
 
+namespace internal {
+
 template<typename T>
 struct is_r_vector : std::false_type {};
 
@@ -322,8 +362,10 @@ struct is_r_vector<r_vec<T>> : std::true_type {};
 template<typename T>
 inline constexpr bool is_r_vector_v = is_r_vector<std::remove_cvref_t<T>>::value;
 
+}
+
 template<typename T>
-concept RVectorType = is_r_vector_v<T>;
+concept RVectorType = internal::is_r_vector_v<T>;
 
 template <RType T>
 inline void r_copy_n(r_vec<T> &target, const T *p_source, R_xlen_t target_offset, R_xlen_t n){
