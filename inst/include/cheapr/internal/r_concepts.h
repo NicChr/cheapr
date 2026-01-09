dac33722ad5a05ba25f3bc7e78a7a47351d58abb
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-01-09 16:25:30 +0000

    Cleaned up concepts a bit.

--- inst/include/cheapr/internal/r_concepts.h
+++ inst/include/cheapr/internal/r_concepts.h
@@ -4,16 +4,25 @@
 #include <cheapr/internal/r_types.h>
 
 namespace cheapr {
+
+template <class... T>
+inline constexpr bool always_false = false;
+
+template <class... T>
+inline constexpr bool always_true = true;
+
 // Compile-time type check `is<>`
 template<typename T, typename U>
     inline constexpr bool is = std::same_as<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
 
+
+template<typename T, typename... Args>
+inline constexpr bool any = (is<T, Args> || ...);
+
 // Concepts to enable R type templates
 
 template<typename T>
-concept RIntegerType = std::same_as<std::remove_cvref_t<T>, r_lgl> ||
-std::same_as<std::remove_cvref_t<T>, r_int> ||
-std::same_as<std::remove_cvref_t<T>, r_int64>;
+concept RIntegerType = any<T, r_lgl, r_int, r_int64>;
 
 template<typename T>
 concept CppIntegerType = std::is_integral_v<std::remove_cvref_t<T>>;
@@ -35,12 +44,7 @@ concept AtLeastOneRMathType =
 (RMathType<T> || RMathType<U>) && (MathType<T> && MathType<U>);
 
 template<typename T>
-concept RType = RMathType<T> ||
-std::same_as<std::remove_cvref_t<T>, r_cplx> ||
-std::same_as<std::remove_cvref_t<T>, r_str> ||
-std::same_as<std::remove_cvref_t<T>, r_raw> ||
-std::same_as<std::remove_cvref_t<T>, r_sym> ||
-std::same_as<std::remove_cvref_t<T>, r_sexp>;
+concept RType = RMathType<T> || any<T, r_cplx, r_str, r_raw, r_sym, r_sexp>;
 
 template<typename T>
 concept CppType = !RType<T>;
@@ -49,15 +53,12 @@ template<typename T, typename U>
 concept AtLeastOneRType = (RType<T> || RType<U>);
 
 template <typename T>
-concept RPtrWritableType = RMathType<T> ||
-is<T, r_cplx> ||
-is<T, r_raw>;
+concept RPtrWritableType = RMathType<T> || any<T, r_cplx, r_raw>;
 
-template <class... T>
-inline constexpr bool always_false = false;
-
-template <class... T>
-inline constexpr bool always_true = true;
+// template <RType T>
+// SEXPTYPE sexp_type(T x){
+// static_assert(always_false<T>, "Unsupported type for `sexp_type`");
+// }
 
 }
 
